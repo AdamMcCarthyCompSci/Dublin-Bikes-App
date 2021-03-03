74a23e608afa6e18660b958b0fb86e64452af68c
Adam McCarthy <adam.mccarthy@ucdconnect.ie>
2021-03-03 16:48:41 +0000

    Night Mode - Fixed

--- static/app.js
+++ static/app.js
@@ -8,44 +8,6 @@ function initMap() {
     .then((data) => {
       map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 53.349804, lng: -6.26031 },
-        zoom: 5,
-      });
-
-      data.forEach((station) => {
-        const circle = new google.maps.Circle({
-          strokeColor: "FF0000",
-          strokeOpacity: "0.8",
-          strokeWeight: 2,
-          fillColor: "ff0000",
-          fillOpacity: 0.35,
-          map: map,
-          center: { lat: station.pos_lat, lng: station.pos_lng },
-          radius: 100,
-        });
-        circle.addListener("click", () => {
-          const infowindow = new google.maps.InfoWindow({
-            content:
-              "<h1>" + station.name + "</h1><b>" + station.bike_stands + "</b>",
-          });
-          infowindow.open(map, circle);
-        });
-      });
-    })
-
-    .catch((err) => {
-      console.log("Error!", err);
-    });
-}
-let map;
-
-function initMap() {
-  fetch("/stations")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 53.349804, lng: -6.26031 },
         zoom: 13,
         styles: [
           { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
