9a3f371b6fb89fc0af594f76c663869b3bc44f97
Adam McCarthy <adam.mccarthy@ucdconnect.ie>
2021-03-03 16:40:28 +0000

    Early version of possible styling for the map

--- static/app.js
+++ static/app.js
@@ -6,23 +6,28 @@ function initMap() {
       return response.json();
     })
     .then((data) => {
-
       map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 53.349804, lng: -6.26031 },
-        zoom: 13,
+        zoom: 5,
       });
 
       data.forEach((station) => {
-        const marker = new google.maps.Marker({
-          position: { lat: station.pos_lat, lng: station.pos_lng },
+        const circle = new google.maps.Circle({
+          strokeColor: "FF0000",
+          strokeOpacity: "0.8",
+          strokeWeight: 2,
+          fillColor: "ff0000",
+          fillOpacity: 0.35,
           map: map,
+          center: { lat: station.pos_lat, lng: station.pos_lng },
+          radius: 100,
         });
-        marker.addListener("click", () => {
+        circle.addListener("click", () => {
           const infowindow = new google.maps.InfoWindow({
             content:
               "<h1>" + station.name + "</h1><b>" + station.bike_stands + "</b>",
           });
-          infowindow.open(map, marker);
+          infowindow.open(map, circle);
         });
       });
     })
