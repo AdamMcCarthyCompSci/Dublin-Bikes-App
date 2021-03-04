7085702881c51be59ae7d000378409a3095f3697
Adam McCarthy <adam.mccarthy@ucdconnect.ie>
2021-03-04 02:14:30 +0000

    Updated style - map boundary, coloured markers, minumum marker sizes.

--- static/app.js
+++ static/app.js
@@ -1,5 +1,16 @@
 let map;
 
+changeCircleColour = (bikes) => {
+  if (bikes == 0) {
+    return "#a83e32";
+  }
+  if (bikes < 5) {
+    return "#a66234";
+  } else {
+    return "#3256cc";
+  }
+};
+
 function initMap() {
   fetch("/stations")
     .then((response) => {
@@ -7,8 +18,17 @@ function initMap() {
     })
     .then((data) => {
       map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 53.349804, lng: -6.26031 },
-        zoom: 13,
+        center: { lat: 53.345804, lng: -6.26031 },
+        restriction: {
+          latLngBounds: {
+            north: 54.345804,
+            south: 52.345804,
+            east: -5.26031,
+            west: -7.26031,
+          },
+          strictBounds: false,
+        },
+        zoom: 14,
         styles: [
           { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
           {
@@ -91,23 +111,35 @@ function initMap() {
             elementType: "labels.text.stroke",
             stylers: [{ color: "#17263c" }],
           },
+          {
+            featureType: "poi",
+            stylers: [{ visibility: "off" }],
+          },
+          // {
+          //   featureType: "transit",
+          //   elementType: "labels.icon",
+          //   stylers: [{ visibility: "off" }],
+          // },
         ],
       });
 
+      // const bikeLayer = new google.maps.BicyclingLayer();
+      // bikeLayer.setMap(map);
+
       data.forEach((station) => {
-        const Circle = new google.maps.Circle({
-          strokeColor: "#4965c1",
+        let Circle = new google.maps.Circle({
+          strokeColor: changeCircleColour(station.available_bikes),
           strokeOpacity: "0.8",
           strokeWeight: 2,
-          fillColor: "#3256cc",
+          fillColor: changeCircleColour(station.available_bikes),
           fillOpacity: 0.35,
           map: map,
           center: { lat: station.pos_lat, lng: station.pos_lng },
-          radius: station.available_bikes * 5,
+          radius: 30 + station.available_bikes * 5,
           clickable: true,
         });
         Circle.addListener("click", () => {
-          console.log("hi,", station.name);
+          console.log("hi,", station.name, station.available_bikes, station);
           // const infowindow = new google.maps.InfoWindow({
           //   content:
           //     "<h1>" + station.name + "</h1><b>" + station.bike_stands + "</b>",
